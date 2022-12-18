40e75003d916f94ea4aee2f16b72b91b95076da0
Alex Werner <obusco@gmail.com>
2022-12-18 02:04:33 +0100

    feat: provide L1/L2 & requestTransaction method

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -7,6 +7,65 @@ class Client {
             address: null,
             index: null
         };
+        this.dashInstance = null;
+    }
+    requestTransaction(opts){
+        const self = this;
+        const EDITOR_EXTENSION_ID = this.CHROME_ID;
+
+        return new Promise((resolve) => {
+            console.log('[Client] Request Transaction ' + JSON.stringify(opts));
+
+            const handleResponse = function (response){
+                console.log(`[Client] ${response.action} - ${JSON.stringify(response.args)}`);
+                resolve(response);
+            }
+
+            chrome.runtime.sendMessage(EDITOR_EXTENSION_ID, {action: "EXECUTE", args: ['TRANSACTION', opts.address, opts.amount]}, handleResponse.bind(this));
+        });
+    }
+    async initDashInstance(){
+        const Dash = require('../../../platform/packages/js-dash-sdk/dist/dash.min');
+        this.dashInstance = new Dash.Client();
+    }
+    async getDocuments(typeLocator, fetchOpts){
+
+        console.log(Dash);
+        const client = new Dash.Client();
+        console.log(client);
+        const getDocuments = async () => {
+            return client.platform.documents.get(typeLocator, fetchOpts);
+        };
+
+        return getDocuments()
+            .then((d) => {
+                for (const n of d) {
+                    console.log('Document:\n', n.toJSON());
+                }
+                return d;
+            })
+            .catch((e) => console.error('Something went wrong:\n', e))
+            .finally(() => client.disconnect());
+
+
+
+        // return client.platform.documents.get(typeLocator, fetchOpts);
+        // const self = this;
+        // const EDITOR_EXTENSION_ID = this.CHROME_ID;
+        //
+        // return new Promise((resolve) => {
+        //     console.log('[Client] Get Documents ' + JSON.stringify({typeLocator,fetchOpts}));
+        //
+        //     const handleResponse = function (response){
+        //         console.log(`[Client] ${response.action} - ${JSON.stringify(response.args)}`);
+        //         resolve(response);
+        //     }
+        //
+        //     chrome.runtime.sendMessage(EDITOR_EXTENSION_ID, {action: "FETCH", args: ['DOCUMENTS', typeLocator, JSON.stringify(fetchOpts)]}, handleResponse.bind(this));
+        // });
+        // const documents = await client.platform.documents.get('tutorialContract.note', {
+        //     limit: 2,
+        // });
     }
 };
 Client.EDITOR_EXTENSION_ID = EDITOR_EXTENSION_ID;
