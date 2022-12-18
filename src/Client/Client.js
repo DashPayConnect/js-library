d5e68c5b0d235ef3de6af6f3a0889003fb5fdd0d
Alex Werner <obusco@gmail.com>
2022-12-18 04:14:56 +0100

    feat: switch current account

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -25,27 +25,29 @@ class Client {
         });
     }
     async initDashInstance(){
-        const Dash = require('../../../platform/packages/js-dash-sdk/dist/dash.min');
-        this.dashInstance = new Dash.Client();
+        if(!chrome.runtime){
+            const Dash = require('../../../platform/packages/js-dash-sdk/dist/dash.min');
+            this.dashInstance = new Dash.Client();
+        }
     }
     async getDocuments(typeLocator, fetchOpts){
 
-        console.log(Dash);
-        const client = new Dash.Client();
-        console.log(client);
-        const getDocuments = async () => {
-            return client.platform.documents.get(typeLocator, fetchOpts);
-        };
-
-        return getDocuments()
-            .then((d) => {
-                for (const n of d) {
-                    console.log('Document:\n', n.toJSON());
-                }
-                return d;
-            })
-            .catch((e) => console.error('Something went wrong:\n', e))
-            .finally(() => client.disconnect());
+        // console.log(Dash);
+        // const client = new Dash.Client();
+        // console.log(client);
+        // const getDocuments = async () => {
+        //     return client.platform.documents.get(typeLocator, fetchOpts);
+        // };
+        //
+        // return getDocuments()
+        //     .then((d) => {
+        //         for (const n of d) {
+        //             console.log('Document:\n', n.toJSON());
+        //         }
+        //         return d;
+        //     })
+        //     .catch((e) => console.error('Something went wrong:\n', e))
+        //     .finally(() => client.disconnect());
 
 
 
@@ -66,6 +68,22 @@ class Client {
         // const documents = await client.platform.documents.get('tutorialContract.note', {
         //     limit: 2,
         // });
+    }
+    switchCurrentAccount(walletId, index){
+        const self = this;
+        const EDITOR_EXTENSION_ID = this.CHROME_ID;
+
+        return new Promise((resolve) => {
+            console.log('[Client] Request Switch ' + JSON.stringify({walletId,index}));
+
+            const handleResponse = async function (response){
+                console.log(`[Client] ${response.action} - ${JSON.stringify(response.args)}`);
+                await self.fetchCurrentAccount();
+                resolve(response);
+            }
+
+            chrome.runtime.sendMessage(EDITOR_EXTENSION_ID, {action: "EXECUTE", args: ['SWITCH_ACCOUNT', walletId, index]}, handleResponse.bind(this));
+        });
     }
 };
 Client.EDITOR_EXTENSION_ID = EDITOR_EXTENSION_ID;
